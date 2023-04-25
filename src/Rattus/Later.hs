f0c14c1fe2716601424681420606f794a8395452
Gregers Rørdam <gregers@rordam.dk>
2023-04-25 07:11:49 +0200

    WIP: improve streams stdlib

--- src/Rattus/Later.hs
+++ src/Rattus/Later.hs
@@ -18,20 +18,22 @@ map f later = delay (f (adv later))
 -- Given a list of delayed values, select over them return list of all available values when one arrives.
 selectMany :: List (O v a) -> O v (List (Int, a))
 selectMany lst = aux 0 lst
-    where
-        aux n (x :! y :! Nil) = 
-            delay (
-                case select x y of
-                    Both a b -> (n, a) :! (n+1, b) :! Nil
-                    Left a lb -> singleton (n, a)
-                    Right la b -> singleton (n+1, b)
-            )
-        aux n (x :! xs) =
-            delay (
-                let xs' = (aux (n+1) xs) in
-                case select x xs' of
-                    Both a b -> (n, a) :! b
-                    Left a lb -> singleton (n, a)
-                    Right la b -> b
-            )
+    --where
         
+
+aux :: Int -> List (O v a) -> O v (List (Int, a))
+aux n (x :! y :! Nil) = 
+    delay (
+        case select x y of
+            Both a b -> (n, a) :! (n+1, b) :! Nil
+            Left a lb -> singleton (n, a)
+            Right la b -> singleton (n+1, b)
+    )
+aux n (x :! xs) =
+    delay (
+        case aux (n+1) xs of
+            xs' -> case select x xs' of
+                Both a b -> (n, a) :! b
+                Left a lb -> (n, a) :! Nil
+                Right la b -> b
+    )  
