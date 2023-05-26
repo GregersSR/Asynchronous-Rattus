39eec71b187a701816b44c0fbd3227e91cf6cfc8
Gregers Rørdam <gregers@rordam.dk>
2023-05-26 10:12:24 +0200

    Add debug prints showing which cells are evaluated

--- examples/simple-sheet/src/Sheet.hs
+++ examples/simple-sheet/src/Sheet.hs
@@ -15,6 +15,7 @@ import AsyncRattus.Strict (List(..), (+++), (:*))
 import qualified Data.Set
 import qualified Data.Map as Map
 import Data.Map ((!))
+import qualified Debug.Trace as D
 
 type Cell = String
 
@@ -32,10 +33,10 @@ inputCells = Strict.fromList ["A1", "A2", "B1", "B2"]
 (a1 :! a2 :! b1 :! b2 :! Nil) = Strict.map' (Stream.fromLater) channels
 
 c1 :: Stream Int
-c1 = Stream.zipWithAwait (box (+)) a1 b1 0 0
+c1 = Stream.zipWithAwait (box (\x y -> D.trace "Evaluating c1" (x+y))) a1 b1 0 0
 
 a3 :: Stream Int
-a3 = Stream.zipWithAwait (box (+)) a1 a2 0 0
+a3 = Stream.zipWithAwait (box (\x y -> D.trace "Evaluating a3" (x+y))) a1 a2 0 0
 
 miniSheet :: Stream (Int :* Int)
 miniSheet = Stream.zip c1 a3
